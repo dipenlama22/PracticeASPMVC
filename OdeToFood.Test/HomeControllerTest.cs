88494f6d087d0428d333aa059d845088047c83b5
dipenlama22 <dipenlama22@gmail.com>
2015-01-04 22:52:00 +0100

    first commit

---
+++ OdeToFood.Test/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+
+namespace OdeToFood.Test
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result.Model);
+        }
+    }
+}
